965aab473954ba8e3deb7c65fa221dbbd738936c
AbhishekGangurdeAdmin <abhirgangurde56@gmail.com>
2025-07-25 16:28:36 +0530

    change message in app.js file

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('This is demo app'));
+app.get('/', (req, res) => res.send('This is demo app for CI/CD pipeline'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
