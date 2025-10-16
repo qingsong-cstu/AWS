5088171b65673b4b7355e5e283f669cb27c45470
Qingsong Zhang <qingsong.zhang@cstu.edu>
2025-10-15 21:14:16 -0700

    Update response message in root route

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello CSTU DevOps! The deploy needs manual approval!. This is the real demo in the class'));
+app.get('/', (req, res) => res.send('Hello CSTU DevOps! The deploy needs manual approval!. This is the real demo in the class. It is really working!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
